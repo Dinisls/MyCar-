ed4118cc3d462b94e78ba599f73bfb6113229a16
Dinisls <dinisls99@gmail.com>
2026-01-16 14:36:31 +0000

    Initial Commit

---
+++ MyCar/MyCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarApp.swift
+//  MyCar
+//
+//  Created by Dinis Santos on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
